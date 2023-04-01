1781b4907abadd5b468393a7c2e4a8a4e3646f2d
abstewart <26328673+abstewart@users.noreply.github.com>
2023-03-31 23:53:24 -0400

    register edits

--- website/src/pages/register/index.js
+++ website/src/pages/register/index.js
@@ -1,10 +1,12 @@
 import NavBar from "../../components/nav-bar";
 
+//lots of examples here https://bootswatch.com/slate/
+
 const Register = () => {
   return(
       <>
         <NavBar active="register"/>
-        <h1>Register</h1>
+        <h1 className={`display-3 text-center`}>Register</h1>
         <div className="form-floating mb-2">
           <input id="register_first_name" type="text" className="form-control"/>
           <label className={`text-dark`} htmlFor={`register_first_name`}>First Name</label>
@@ -22,7 +24,7 @@ const Register = () => {
           <label className={`text-dark`} htmlFor={`register_username`}>Username</label>
         </div>
         <div className="mt-3">
-          <div>Password must be 8-32 characters in length, and include 1 number and one of '!@#$%&*</div>
+          <div className={`text-info`}>Password must be 8-32 characters in length, include 1 number, and one of ['!@#$%&*]</div>
           <div className="form-floating">
             <input id="register_password_1" type="password" className="form-control"/>
             <label className={`text-dark`} htmlFor={`register_password_1`}>Password</label>
@@ -30,7 +32,7 @@ const Register = () => {
 
         </div>
         <div className="mt-3">
-          <label htmlFor={`register_show_password`}>Show password: &nbsp;</label>
+          <label className={`text-info`} htmlFor={`register_show_password`}>Show password: &nbsp;</label>
           <input type={`checkbox`} id={`register_show_password`}/>
         </div>
         <div className="form-floating">
