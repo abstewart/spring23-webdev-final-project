03ac89b388def974fe52e13b66e503b789cc0691
abstewart <26328673+abstewart@users.noreply.github.com>
2023-03-31 23:16:20 -0400

    basic nav-bar

--- website/src/components/nav-bar/index.js
+++ website/src/components/nav-bar/index.js
@@ -4,7 +4,67 @@
 
 import {Link} from "react-router-dom";
 
+
 const NavBar = () => {
+  return(
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/home">Website!</Link>
+            <div className="collapse navbar-collapse" id="navbarColor01">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/home">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/register">Register</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/search">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/profile">Profile</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/profile">Profile</a>
+                </li>
+                <li className="nav-item">
+                  <div className="nav-fill">
+                    <span>Welcome:&nbsp;</span>
+                    <span>Anonymous</span>
+                  </div>
+                </li>
+                {/*<li className="nav-item dropdown">*/}
+                {/*  <a className="nav-link dropdown-toggle"*/}
+                {/*     data-bs-toggle="dropdown" href="#" role="button"*/}
+                {/*     aria-haspopup="true" aria-expanded="false">Dropdown</a>*/}
+                {/*  <div className="dropdown-menu">*/}
+                {/*    <a className="dropdown-item" href="#">Action</a>*/}
+                {/*    <a className="dropdown-item" href="#">Another action</a>*/}
+                {/*    <a className="dropdown-item" href="#">Something else*/}
+                {/*      here</a>*/}
+                {/*    <div className="dropdown-divider"></div>*/}
+                {/*    <a className="dropdown-item" href="#">Separated link</a>*/}
+                {/*  </div>*/}
+                {/*</li>*/}
+              </ul>
+              <div className="d-flex">
+                <input className="form-control me-sm-2" type="text"
+                       placeholder="Search"/>
+                <button className="btn btn-secondary my-2 my-sm-0"
+                        type="button">Search
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+  );
+}
+const NavBar1 = () => {
   return(
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <a className="navbar-brand text-light" href="#">Navbar</a>
