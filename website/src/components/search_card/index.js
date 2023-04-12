9b4ecdebfbd9010b545d9ec0a21f11fc8c276624
abstewart <26328673+abstewart@users.noreply.github.com>
2023-04-11 23:33:55 -0400

    add nps-service file for using the service api from middleware server

--- website/src/components/search_card/index.js
+++ website/src/components/search_card/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import {getParkDetails} from "../../services/nps/nps-service";
 
 export const SearchParkDetailsCard = (
     {
@@ -7,9 +8,10 @@ export const SearchParkDetailsCard = (
     const [park, setPark] = useState(null);
     useEffect(() => {
         const fetchPark = async () => {
-            const response = await fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=CsPUxsJjlI83u6XabTVnAbX5kCQXBDv9HJQcpUUM`);
-            const data = await response.json();
-            setPark(data.data[0]);
+            // const response = await fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=CsPUxsJjlI83u6XabTVnAbX5kCQXBDv9HJQcpUUM`);
+            // const data = await response.json();
+          const data = await getParkDetails(parkCode);
+          setPark(data.data[0]);
         };
         fetchPark();
     }, [parkCode]);
---
+++ website/src/services/nps/nps-service.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const NPS_API = `${API_BASE}/parks`;
+
+export const getParkDetails = async (parkId) => {
+  const response = await axios.get(`${NPS_API}/getPark/${parkId}`);
+  //console.log(response.data.data);
+  return response.data;
+}
+
+export const generalParkSearch = async ({state, term}) => {
+  const response = await axios.get(`${NPS_API}/search?stateCode=${state}&searchTerm=${term}`)
+}
