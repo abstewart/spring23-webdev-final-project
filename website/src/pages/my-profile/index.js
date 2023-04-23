dddbe8cf7caac6e16f86d17ec29ecc7fa130ce09
abstewart <26328673+abstewart@users.noreply.github.com>
2023-04-23 15:30:02 -0400

    add redirect to myProfile page if not logged in

--- website/src/pages/my-profile/index.js
+++ website/src/pages/my-profile/index.js
@@ -1,7 +1,18 @@
 import MPDetails from "./mp-details";
+import {useEffect} from "react";
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 
 const MyProfile = () => {
+  const { currentUser } = useSelector((state) => state.users);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!currentUser){
+      navigate("/login")
+    }
+  }, [])
+
   return(
       <>
         <div>
